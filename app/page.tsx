079f3552121fcc6bcd8525f930d790aba9b6bd61
Gilad <giladwilk@yahoo.com>
2025-06-30 18:39:43 +0300

    Fix: Add 'use client' directive for interactive carousel component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { Download, Star, Users, Zap, Shield, BarChart3, Chrome, ArrowRight, ChevronLeft, ChevronRight, Play } from 'lucide-react'
